5b19f0af062ada91eca558a97c603a5e1ab8a4df
Mattegoat <matteo.georges74@hotmail.fr>
2022-12-19 01:32:22 +0100

    feat: theme picker

--- src/pages/theme.tsx
+++ src/pages/theme.tsx
@@ -89,6 +89,67 @@ const Theme: NextPage = () => {
 						<div className="overflow-hidden w-7/12 card mx-auto">
 							<Image src={nouns} />
 						</div>
+						<div className="flex flex-row gap-4 m-auto pt-4 w-fit">
+							<div className="btn btn-outline btn-accent btn-square false">
+								<svg
+									width="24"
+									height="24"
+									xmlns="http://www.w3.org/2000/svg"
+									fillRule="evenodd"
+									clipRule="evenodd"
+									style={{ transform: 'rotate(180deg)' }}
+								>
+									<path
+										fill="hsl(var(--p))"
+										d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+									/>
+								</svg>
+							</div>
+							<div className="btn btn-outline btn-accent btn-square false">
+								<svg
+									width="24"
+									height="24"
+									xmlns="http://www.w3.org/2000/svg"
+									fillRule="evenodd"
+									clipRule="evenodd"
+								>
+									<path
+										fill="hsl(var(--p))"
+										d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+									/>
+								</svg>
+							</div>
+						</div>
+						<h1 className="text-lg sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl font-bold text-center pt-4">
+							Purple DAO #39
+						</h1>
+						<div className="flex flex-col gap-4 justify-center w-full">
+							<div className="flex flex-row gap-2 sm:gap-4 md:gap-10 m-auto pt-4">
+								<div className="flex flex-col gap-1">
+									<span className="text-md font-semibold text-secondary">Current Bid</span>
+									<div className="text-lg md:text-xl lg:text-3xl xl:text-4xl font-bold">2 Ξ </div>
+								</div>
+								<div className="divider divider-horizontal color-primary" />
+								<div className="flex flex-col gap-1">
+									<div className="text-md font-semibold text-secondary">Auction ends in</div>
+									<div className="text-lg md:text-xl lg:text-3xl xl:text-4xl font-bold">
+										{' '}
+										2h 3mn 10s
+									</div>
+								</div>
+							</div>
+							<div className="form-contro pt-4 m-auto">
+								<div className="input-group">
+									<input
+										type="text"
+										pattern="[0-9.]*"
+										placeholder={`2.3 Ξ or more`}
+										className="input input-bordered font-bold w-full lg:w-7/12"
+									/>
+									<button className="btn btn-square px-2 bg-primary">Bid</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

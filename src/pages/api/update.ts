52c01d5caf9fd1ed8f15138fda31ca89a6de40f5
Evolve <Artevolve@yahoo.com>
2022-12-19 17:54:36 +0100

    fix: /update 🔨

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			const files = [
 				{
 					name: 'config/config.json',
-					contents: JSON.stringify(config),
+					contents: JSON.stringify({ ...config, adminAddress: '', chainId: '1' }),
 				},
 			]
 

79d829509ff519994a69736e364da4e4e2904f4c
Mattegoat <matteo.georges74@hotmail.fr>
2022-12-19 13:35:53 +0100

    fix: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	daisyui: {
-		styled: true,
-		themes: true,
-		base: true,
-		utils: true,
-		// logs: true,
-		rtl: false,
-		prefix: '',
-		darkTheme: 'black',
-	},
-	darkMode: 'class',
-	theme: {
-		extend: {},
+		themes: ['black', 'black'],
 	},
 	plugins: [require('daisyui')],
 }
